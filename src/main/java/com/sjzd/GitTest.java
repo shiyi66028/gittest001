067819c38c8564b140b4f7fd1743e4b3fb1077b5
shiyi66028 <shiyi66028@126.com>
2021-09-07 09:43:35 +0800

    first commit

---
+++ src/main/java/com/sjzd/GitTest.java
@@ -0,0 +1,13 @@
+package com.sjzd;
+
+/**
+ * @author YSS
+ * @version V1.0
+ * @package com.sjzd
+ * @date 2021/9/7 9:40
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
